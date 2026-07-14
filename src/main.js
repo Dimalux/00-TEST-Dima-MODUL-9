7c47dc56cbbaa776d612c0bb9c93c36254313d44
Dima_V <astrallux777@ukr.net>
2026-07-14 18:03:33 +0300

    00-TEST-Dima-MODUL-9-21

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
-import { nanoid } from 'nanoid'
 
 
-console.log(nanoid());
+
+// ПОЯСНЕННЯ-20    npm ПАКЕТИ  (01:30:20 / 01:31:20)   (дивись рядок 1521 в файлі  "main Test.js")
+
+// import { nanoid } from 'nanoid'
+
+// console.log(nanoid());
 
 // 1) запускаємо проект :
 // npm run dev
@@ -10,4 +14,11 @@ console.log(nanoid());
 
 // import { nanoid } from 'nanoid'    -   імпорт м/б на самій горі файлу main.js
 // console.log(nanoid());   //   в консолі при кожному перезавантаженні сторінки пакет "nanoid" буде генерувати випадкові ID-ки
- 
+
+//..................
+
+// ПОЯСНЕННЯ-21 :
+
+import sum from "./helpers/helpers"
+
+console.log(sum(15, 25));
