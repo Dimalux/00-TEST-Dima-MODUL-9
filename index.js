5cffcbe618a6524f1787e6a1afd90a3a17377227
Dimalux <astrallux777@ukr.net>
2026-07-26 20:29:38 +0000

    Deploying to gh-pages from @ Dimalux/00-TEST-Dima-MODUL-9@947393f86aabb8302101c510e4ec7ff2173ddf5a 🚀

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-import{u as o,a as l}from"./assets/helpers-DtsAHaln.js";/* empty css                      */console.log(o);console.log(l());
+import{u as o,a as l}from"./assets/helpers-DtsAHaln.js";/* empty css                      */console.log(o);console.log(l());console.log("First log");setTimeout(()=>{console.log("Second log")},2e3);console.log("Third log");
 //# sourceMappingURL=index.js.map
